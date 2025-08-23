da9b90352aa2344430771ab900588472185f1757
Padawan222 <emahobaudorre@gmail.com>
2025-08-23 22:03:07 +0200

    first commit

--- src/index/index.js
+++ src/index/index.js
@@ -4,14 +4,30 @@ import "./index.scss";
 const input = document.querySelector(".container-blur input");
 const containerBlur = document.querySelector(".container-blur");
 const opacity = document.querySelector('.opacity')
+const text1 = document.querySelector('.text-1')
+const text2 = document.querySelector('.text-2')
 
 // ✅ Tu peux ajouter autant de phrases que tu veux ici
-const validPhrases = ["test password"," Moi c'est toi", "moi c'est toi"];
+const validPhrases1 = ["182 jours"];
 
 input.addEventListener("input", () => {
   const value = input.value.trim().toLowerCase();
 
-  if (validPhrases.includes(value)) {
+  if (validPhrases1.includes(value)) {
+ 
+      text1.style.display = "none";
+    text2.style.display = "block";
+    input.value = ""; // 🔥 supprime le texte de l’input
+
+  } else {
+  }
+});
+const validPhrases2 = ["test password"," Moi c'est toi", "moi c'est toi"];
+
+input.addEventListener("input", () => {
+  const value = input.value.trim().toLowerCase();
+
+  if (validPhrases2.includes(value)) {
     containerBlur.classList.add("no-blur");
     opacity.classList.add('opacity-active')
     setTimeout(()=>{
